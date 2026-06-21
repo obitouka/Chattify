0f82d4f319d391899770d2b0ee1c4bf14862217e
obitouka <obitouka@proton.me>
2026-06-21 16:28:32 +0530

    renamed project to "chattify"

--- chattify/chattify/urls.py
+++ chattify/chattify/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for studybud project.
+URL configuration for chattify project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
