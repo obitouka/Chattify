792ba46fd01642b861108befdf0cdc89af6abdf6
obitouka <obitouka@proton.me>
2026-06-22 23:05:29 +0530

    Implement: theme installation, user profile edit page

--- chattify/base/views.py
+++ chattify/base/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 # rooms = [
 #     {'id':1, 'name':'Let\'s learn python'},
 #     {'id':2, 'name':'Design with me'},
@@ -109,15 +109,25 @@ def userProfile(request, pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit= False)
-            room.host = room.user
-            room.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        Room. objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description'),
+        )
 
-    context = {'form' : form}
+        # form = RoomForm(request.POST)
+        # if form.is_valid():
+        #     room = form.save(commit= False)
+        #     room.host = request.user
+        #     room.save()
+        #     return redirect('home')
+
+    context = {'form' : form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 
@@ -125,17 +135,23 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse('You are not allowed here')
 
     if request.method == "POST":
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('descripton')
+        room.save()
+        # form = RoomForm(request.POST, instance=room)
+        # if form.is_valid():
+        #     form.save()
+        return redirect('home')
     
-    context = {'form' : form}
+    context = {'form' : form, 'topics': topics, 'room': room}
     return render(request, 'base/room_form.html', context)
 
 
@@ -160,3 +176,20 @@ def deleteMessage(request, pk):
         return redirect('home')
 
     return render(request, 'base/delete.html', {'obj': message})
+
+
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST' :
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    context = {'form':form}
+    return render(request, 'base/update-user.html', context)
+
