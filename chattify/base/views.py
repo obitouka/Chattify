8adef992aacf6a047add91f4a838dddae3d89611
obitouka <obitouka@proton.me>
2026-06-24 01:19:09 +0530

    Add REST API

--- chattify/base/views.py
+++ chattify/base/views.py
@@ -62,18 +62,18 @@ def registerPage(request):
 
 def home(request):
     q = request.GET.get('q')  if request.GET.get('q') != None else ''
-
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
         Q(description__icontains=q)
     ) 
     room_messages = Messages.objects.filter(Q(room__topic__name__icontains=q))
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[:5]
     room_count = rooms.count()
 
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
+
 
 
 def room(request, pk):
@@ -97,6 +97,8 @@ def room(request, pk):
     context = {"room": room, 'room_messages': room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
 
+
+
 def userProfile(request, pk):
     user = User.objects.get(id=pk)
     rooms = user.room_set.all()
@@ -104,6 +106,7 @@ def userProfile(request, pk):
     topics = Topic.objects.all()
     context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
     return render(request, 'base/profile.html', context)
+
 
 
 @login_required(login_url='login')
@@ -114,7 +117,6 @@ def createRoom(request):
     if request.method == "POST":
         topic_name = request.POST.get('topic')
         topic, created = Topic.objects.get_or_create(name=topic_name)
-
         room = Room.objects.create(
             host=request.user,
             topic=topic,
@@ -122,16 +124,15 @@ def createRoom(request):
             description=request.POST.get('description'),
         )
         return redirect('room', pk = room.id)
-        
         # form = RoomForm(request.POST)
         # if form.is_valid():
         #     room = form.save(commit= False)
         #     room.host = request.user
         #     room.save()
         #     return redirect('home')
-
     context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
+
 
 
 @login_required(login_url='login')
@@ -153,7 +154,6 @@ def updateRoom(request, pk):
         # if form.is_valid():
         #     form.save()
         return redirect('room', pk = room.id)
-    
     context = {'form' : form, 'topics': topics, 'room': room}
     return render(request, 'base/room_form.html', context)
 
@@ -167,17 +167,18 @@ def deleteRoom(request, pk):
     return render(request, 'base/delete.html', {'obj':room})
 
 
+
 @login_required(login_url='login')
 def deleteMessage(request, pk):
     message = Messages.objects.get(id=pk)
 
     if request.user != message.user:
         return HttpResponse('Your are not allowed here!!')
-
+    
     if request.method == 'POST':
         message.delete()
         return redirect('home')
-
+    
     return render(request, 'base/delete.html', {'obj': message})
 
 
@@ -196,3 +197,17 @@ def updateUser(request):
     context = {'form':form}
     return render(request, 'base/update-user.html', context)
 
+
+
+def topicsPage(request):
+    q = request.GET.get('q')  if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    
+    context = {'topics':topics}
+    return render(request, 'base/topics.html', context)
+
+def activityPage(request):
+    room_messages = Messages.objects.all()[:10]
+    
+    context = {'room_messages':room_messages}
+    return render(request, 'base/activity.html', context)
--- chattify/chattify/settings.py
+++ chattify/chattify/settings.py
@@ -43,10 +43,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
+
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,3 +135,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+
+# CORS_ALLOWED_ORIGINS
+# CORS_ALLOWED_ORIGIN_REGEXES
+
+CORS_ALLOW_ALL_ORIGINS = True
